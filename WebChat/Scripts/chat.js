43e4911472ef33e181305e5232d934ca86096e9f
Azamat Atashov <ronixtey@gmail.com>
2022-10-27 00:17:24 +0600

    Add project files.

---
+++ WebChat/Scripts/chat.js
@@ -0,0 +1,43 @@
+﻿const chat = $.connection.chatHub;
+
+function AppViewModel() {
+    this.name = ko.observable('');
+    this.isMessageEmpty = ko.observable(false);
+
+    this.clickHandler = () => {
+        if (!this.name())
+            return this.name(prompt('Enter your name:', ''));
+        if (!$('#message').val())
+            return this.isMessageEmpty(true);
+
+        chat.server.send(this.name(), $('#message').val());
+        $('#message').val('').focus();
+    };
+}
+
+$.connection.hub
+    .start()
+    .done(() => {
+        const viewModel = new AppViewModel();
+        ko.applyBindings(viewModel);
+        // фокусироваться каждый раз на input, после ввода имени
+        viewModel.name.subscribe(() => $('#message').focus());
+
+        viewModel.name(prompt('Enter your name:', ''));
+    });
+
+
+chat.client.send = (name, message) => {
+    $('#discussion').append(`
+        <li>
+            (${new Date().toLocaleTimeString()}) <strong> ${htmlEncode(
+        name
+    )}</strong >: ${htmlEncode(message)}
+        </li>
+    `);
+};
+
+function htmlEncode(value) {
+    var encodedValue = $('<div />').text(value).html();
+    return encodedValue;
+}
---
+++ WebChat/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(WebChat.Startup))]
+
+namespace WebChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
